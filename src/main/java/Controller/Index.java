7f215244a05f6a6426b1953af680b0cd58405afa
mbaye <mmbaye1@hotmail.fr>
2024-05-18 15:38:52 +0200

    mis en place de la BD REUSSIE

--- src/main/java/Controller/Index.java
+++ src/main/java/Controller/Index.java
@@ -1,11 +1,16 @@
 package Controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Models.Db;
 
 /**
  * Servlet implementation class Index
@@ -19,14 +24,14 @@ public class Index extends HttpServlet {
      */
     public Index() {
         super();
-        System.out.println("First");
+        Db.connect();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,8 +6,6 @@
 
 </head>
 <body>
-	<c:set var="age" value="0" />	
-	<c:if test="${age ge 18}"> <p>Vous êtes majeur.</p></c:if>
-	<c:if test="${age lt 18}"> <p>Vous êtes mineur.</p></c:if>
+	
 </body>
 </html>
